7dd386e854cd092b6602ad8606cf0b2606038394
oliule2010 <oliule2010@gmail.com>
2021-04-03 14:18:06 +0100

    add count appearance

--- 4thApril2021/4thApril2021.cpp
+++ 4thApril2021/4thApril2021.cpp
@@ -17,8 +17,32 @@ vector<int> get_divide_by3(vector<int>& numbers) {
     return output;
 }
 
+
+void display_vector(const vector<int>& numbers) {
+    for (int value : numbers) {
+        cout << value << endl;
+    }
+}
+
+// find the number of appearance of x in numbers
+int count_appearance(const vector<int>& numbers, int x) {
+    int count = 0;
+    for (int value : numbers) {
+        if (value == x) {
+            count++;
+        }
+    }
+    return count;
+}
+
+
 int main()
 {
-    vector<int> numbers = { 1,4, 3, 2, 6, 9, 15, 32 };
+    vector<int> numbers = { 1,4, 3, 2, 6, 3, 15, 32 };
+    vector<int> results = get_divide_by3(numbers);
+    display_vector(results);
+    cout << "number appearance of 3 " << count_appearance(numbers, 3) << endl;
+    cout << "number appearance of 15 " << count_appearance(numbers, 15) << endl;
+    cout << "number appearance of 25 " << count_appearance(numbers, 25) << endl;
 
 }
